48455a437844704e5c0641428515bd72cb015b66
Sreyas Sabu Chacko <schacko@umd.edu>
2022-03-03 17:08:23 -0500

    PacMan Test (Valid Moves) Draft 1

--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -5,6 +5,16 @@ import java.io.*;
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		//Creating A Map
+		NoFrame frame2 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+		//Creating Players
+		PacMan pacman2 = frame2.addPacMan(new Location(3, 3));
+
+		assertTrue(pacman2.get_valid_locs().size() == 3);
+		assertTrue(pacman2.get_valid_locs().contains(new Location(2, 3)));
+		assertTrue(pacman2.get_valid_locs().contains(new Location(4, 3)));
+		assertTrue(pacman2.get_valid_locs().contains(new Location(3, 4)));
+		
 	}
 }
