a19857b6c286db6e653940906c25e1ea5417503a
Sreyas Sabu Chacko <schacko@umd.edu>
2022-03-03 17:19:05 -0500

    Ghost Test (Valid Moves) Draft 1

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -5,6 +5,14 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		//Creating A Map
+		NoFrame frame2 = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+		Ghost ghost = frame.addGhost(new Location(3, 3), "red", Color.red);
+
+		assertTrue(ghost.get_valid_locs().size() == 3);
+		assertTrue(ghost.get_valid_locs().contains(new Location(2, 3)));
+		assertTrue(ghost.get_valid_locs().contains(new Location(4, 3)));
+		assertTrue(ghost.get_valid_locs().contains(new Location(3, 4)));
 	}
 }
