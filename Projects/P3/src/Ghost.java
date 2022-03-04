c087c4f57a469232a2544efbad5fcbd7fd185fb0
Darren Kam <darrenmkam4150@gmail.com>
2022-03-04 05:28:51 -0500

    Implement Ghost is_pacman_in_range()

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -20,7 +20,12 @@ public class Ghost{
 		return false;
 	}
 
-	public boolean is_pacman_in_range() { 
+	public boolean is_pacman_in_range() {
+		for(Location loc : get_valid_moves()) {
+			if(myMap.getLoc(loc).contains(Map.Type.PACMAN)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
