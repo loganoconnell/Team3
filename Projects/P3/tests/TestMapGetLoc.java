b888b664ed61195101756f51ae4b361add672e75
Sreyas Sabu Chacko <schacko@umd.edu>
2022-03-04 21:18:44 -0500

    Test Map getLoc Fix 2

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -9,7 +9,7 @@ public class TestMapGetLoc {
 	public void testMapGetLoc() {
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
-		assertTrue(frame.getLoc(3,3) == Map.Type.EMPTY);
-		assertTrue(frame.getLoc(3,2) == Map.Type.WALL);
+		assertTrue(frame.getMap().getLoc(3,3) == Map.Type.EMPTY);
+		assertTrue(frame.getMap().getLoc(3,2) == Map.Type.WALL);
 	}
 }
