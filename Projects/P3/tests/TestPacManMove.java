83e685332f16431bc37425d25bd01cfa92aee0d3
Darren Kam <darrenmkam4150@gmail.com>
2022-03-14 21:39:26 -0400

    Use NoFrame instead of MainFrame for junit tests

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -5,9 +5,9 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		MainFrame mf = new MainFrame();
+		NoFrame nf = new NoFrame();
 
-		PacMan pc = mf.addPacMan(new Location(1, 1));
+		PacMan pc = nf.addPacMan(new Location(1, 1));
 
 		assertTrue(pc.move());
 		assertTrue(pc.myLoc.equals(new Location(1, 2)) || pc.myLoc.equals(new Location(2, 1)));
