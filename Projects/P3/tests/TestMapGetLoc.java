0216d16ff355c07ffca74ecf233f021385762874
Sreyas Sabu Chacko <schacko@umd.edu>
2022-03-15 10:37:00 -0400

    modify test

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -4,12 +4,12 @@ import java.awt.Color;
 import java.io.*;
 
 
-public class TestMapGetLoc {
+public class TestMapGetLoc extends TestCase{
 	
 	public void testMapGetLoc() {
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
-		assertTrue(frame.getMap().getLoc(3,3) == Map.Type.EMPTY);
-		assertTrue(frame.getMap().getLoc(3,2) == Map.Type.WALL);
+		assertTrue(frame.getMap().getLoc(new Location(3,3)).contains(Map.Type.EMPTY));
+		assertTrue(frame.getMap().getLoc(new Location(3,2)).contains(Map.Type.WALL));
 	}
 }
