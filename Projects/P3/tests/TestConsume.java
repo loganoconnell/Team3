1d8cfb96a493929c59c52ca417603b781fee6c5d
Ishaan <ishaanb04@gmail.com>
2022-03-17 17:12:35 -0400

    Updated main.yml Ishaan

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -5,6 +5,15 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		NoFrame nf = new NoFrame();
+		Map map = nf.getMap();
+
+		PacMan pacman = nf.addPacMan(new Location(1, 1));
+
+		assertTrue(map.getLoc(pacman.myLoc).contains(Map.Type.COOKIE));
+		assertNotNull(pacman.consume());
+
+		assertFalse(map.getLoc(pacman.myLoc).contains(Map.Type.COOKIE));
+		assertNull(pacman.consume());
 	}
 }
--- Projects/P3/tests/TestMapAttack.java
+++ Projects/P3/tests/TestMapAttack.java
@@ -4,6 +4,15 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		NoFrame nf = new NoFrame();
+		Map map = nf.getMap();
+
+		PacMan pacman = nf.addPacMan(new Location(1, 1));
+
+		Ghost blinky = nf.addGhost(new Location(1, 2), "Blinky", Color.red);
+		Ghost clyde = nf.addGhost(new Location(1, 3), "Clyde", Color.orange);
+
+		assertTrue(map.attack("Blinky"));
+		assertFalse(map.attack("Clyde"));
 	}
 }
